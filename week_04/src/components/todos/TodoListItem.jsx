6e1711b28328209dbbecc2c17356e1a6cd979a8c
pgm-dirkbouckaert <dirk.bouckaert@student.arteveldehs.be>
2024-05-07 13:26:26 +0200

    Update CSS

--- week_04/src/components/todos/TodoListItem.jsx
+++ week_04/src/components/todos/TodoListItem.jsx
@@ -1,8 +1,8 @@
 const TodoListItem = ({ todo }) => {
   return (
-    <li className="my-4 rounded-lg border border-blue-600 p-4">
-      <h2 className="text-blue-600">{todo.title}</h2>
-      <p className="text-slate-500">{todo.description}</p>
+    <li className="my-4 rounded-lg border border-blue-600 p-4 dark:border-blue-400">
+      <h2 className="text-blue-600 dark:text-blue-400">{todo.title}</h2>
+      <p className="text-slate-500 dark:text-slate-400">{todo.description}</p>
     </li>
   );
 };
